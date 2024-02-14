1d783a6834625704d0de05d4fc5647dbd40b3bd9
dzomboallan <dzomboallan1809@gmail.com>
2024-02-14 14:59:08 +0300

    Fixed bugs

--- DjangoDocker/settings.py
+++ DjangoDocker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-i9b-1!rjhpd(t(rf1c9gxw6ygtqkk7wcmr47kq43!xd1&xen&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
